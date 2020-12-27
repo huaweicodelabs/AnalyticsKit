0cf06b169da28c6b08af24a1fc57ae6c81d6f2fc
yang liu <yangliu106@huawei.com>
2020-12-28 01:01:39 +0800

    first commit

---
+++ Android/Analytics_android_SampleCode_Kotlin/code/MainActivity.kt
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2020. Huawei Technologies Co., Ltd. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package com.huawei.hmsanalyticskitdemokotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+// TODO: import classes from Analytics Kit
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnSetting: Button
+
+    private val questions = arrayOf(R.string.q1, R.string.q2, R.string.q3, R.string.q4, R.string.q5)
+    private val answers = arrayOf(true, true, false, false, true)
+
+    private var curQuestionIdx = 0
+
+    private lateinit var txtQuestion: TextView
+
+    private lateinit var btnNext: Button
+
+    private lateinit var btnTrue: Button
+
+    private lateinit var btnFalse: Button
+
+    private lateinit var postScore: Button
+
+    private var score = 0
+
+    // TODO: Define a var for Analytics Instance
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // TODO: Initiate Analytics Kit
+
+        txtQuestion = findViewById(R.id.question_text_view)
+        txtQuestion.setText(questions[curQuestionIdx])
+
+        btnSetting = findViewById(R.id.setting_button)
+        btnSetting.setOnClickListener { view ->
+            var i = Intent(this, SettingActivity::class.java)
+            startActivityForResult(i, 0)
+        }
+
+        btnNext = findViewById(R.id.next_button)
+        btnNext.setOnClickListener {
+            curQuestionIdx = (curQuestionIdx + 1) % questions.size
+            nextQuestion()
+        }
+
+        btnTrue = findViewById(R.id.true_button)
+        btnTrue.setOnClickListener {
+            checkAnswer(true)
+            reportAnswerEvt("true")
+        }
+
+        btnFalse = findViewById(R.id.false_button)
+        btnFalse.setOnClickListener {
+            checkAnswer(false)
+            reportAnswerEvt("false")
+        }
+
+        postScore = findViewById(R.id.post_score_button)
+        postScore.setOnClickListener {
+            postScore()
+        }
+    }
+
+    private fun nextQuestion() {
+        txtQuestion.setText(questions[curQuestionIdx])
+    }
+
+    private fun checkAnswer(answer: Boolean): Boolean {
+        val q = txtQuestion.text.toString().trim()
+        if (answer == answers[curQuestionIdx]) {
+            score = score + 20
+            Toast.makeText(this, R.string.correct_answer, Toast.LENGTH_SHORT).show()
+            // TODO: Report a customized Event
+
+        } else {
+            Toast.makeText(this, R.string.wrong_answer, Toast.LENGTH_SHORT).show()
+            // TODO: Report a customized Event
+
+        }
+        return answers[curQuestionIdx]
+    }
+
+    private fun reportAnswerEvt(answer: String) {
+        // TODO: Report a customzied Event
+        // Event Name: Answer
+        // Event Parameters:
+        //  -- question: String
+        //  -- answer:String
+        //  -- answerTime: String
+    }
+
+    private fun postScore() {
+        // TODO: Report score by using SUBMITSCORE Event
+    }
+}
---
+++ Android/CodeLab_Analytics_Demo_Kotlin/readme.md
@@ -0,0 +1,70 @@
+# HMS AnalyticsKit Demo for Android
+
+
+## Table of Contents
+
+* [Introduction](#introduction)
+* [Getting Started](#getting-started)
+* [Installation](#installation)
+* [Configuration ](#configuration )
+* [Environment requirements](#environment-requirements)
+* [Result](#result)
+* [License](#license)
+
+
+## Introduction
+Hmsanalyticskitdemo provides an example of integrating HUAWEI Analytics Kit with an app. It illustrates how to collect predefined events and custom events to improve user engagement and user preference.
+[Read more about Hianalytics](https://developer.huawei.com/consumer/en/doc/development/HMSCore-Guides-V5/introduction-0000001050745149).
+
+## Getting Started
+
+For more development guidance, please refer to the links below:
+
+[Development Guide](https://developer.huawei.com/consumer/en/doc/development/HMSCore-Guides-V5/android-dev-process-0000001050163813).
+
+[API References](https://developer.huawei.com/consumer/en/doc/development/HMSCore-References-V5/android-api-analytics-overview-0000001051067140).
+
+In this example, we demonstrate how to use the analytics SDK for Android by using the Gradle build system.
+
+First download Hmsanalyticskitdemo by cloning this repository or downloading an archived snapshot.
+
+In Android Studio, click Open an existing Android Studio project, and select analytics-sample.
+
+You can use the gradlew build command to build the project directly.
+
+You should create an app in AppGallery Connect, and obtain the agconnect-services.json file and add it to the project. You should also generate a signing certificate fingerprint and add the certificate file to the project, and add configuration to the build.gradle file.  More to [Development Process](https://developer.huawei.com/consumer/en/doc/development/HMSCore-Guides-V5/android-dev-process-0000001050163813).
+
+
+## Installation
+Use Android Studio to open the decompressed project.
+
+## Configuration
+1. Create an app in AppGallery Connect and obtain the project configuration file agconnect-services.json. In Android Studio, switch to the Project view and move the agconnect-services.json file to the root directory of the app.
+2. Change the value of applicationId in the build.gradle file of the app to the name of the app package applied for in the preceding step.
+
+## Environment requirements
+Hardware requirements:
+1. A computer (desktop or laptop)
+2. A Huawei phone, which is used to debug the developed app
+  
+Software requirements:
+1. HUAWEI Analytics Kit 5.1.0
+2. Android SDK applicable to devices using Android 4.4 KitKat (API level 19) and later versions
+3. Android Studio 
+4. Java JDK 1.7 (JDK 1.8 is recommended.)
+
+## Result
+After running the app you should see a screen like this:
+
+<img src="images/screen_0.png" height="534" width="300" style="max-width:100%;">
+
+Click TRUE or FALSE to answer the question; Click NEXT to show the next question; Click POST SCORE, log the score user got. All the information will be uploaded to the Hianalytics Console, and you can see these information in real time using Debug View.
+
+Click button SETTINGS:
+
+<img src="images/screen_1.png" height="534" width="300" style="max-width:100%;">
+
+When you click SETTINGS, you will be asked for your favorite sport. Your choice will be logged by Hianalytics as a user attribute.
+
+##  License
+HmsAnalyticsKitDemo is licensed under the [Apache License, version 2.0](http://www.apache.org/licenses/LICENSE-2.0).
